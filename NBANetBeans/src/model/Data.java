8c167ef340bebc32ef9a9f6e436f8cb3cafadccb
wpoliciano <w.policiano@gmail.com>
2015-06-24 23:51:13 -0300

    Adicionado Classe temporaria que simula o banco em memoria.

---
+++ NBANetBeans/src/model/Data.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author will
+ */
+public class Data {
+    public static List<Person> listPerson = new ArrayList<>();
+}
