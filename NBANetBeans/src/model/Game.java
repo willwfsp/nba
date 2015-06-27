44c78e7c69fe57a38f61054496bb758ea226a8e2
wpoliciano <w.policiano@gmail.com>
2015-06-26 23:49:27 -0300

    Adicionado a classe Game que receberá uma visão.

---
+++ NBANetBeans/src/model/Game.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+
+/**
+ * Esta classe receberá os dados da view GameView
+ * @author will
+ */
+public class Game implements Serializable{
+    private Integer numGame;
+    private Calendar date;
+    private Calendar hour;
+    private Integer arena;
+    private Integer Season;
+
+    public Game() {
+    }
+
+    public Integer getNumGame() {
+        return numGame;
+    }
+
+    public void setNumGame(Integer numGame) {
+        this.numGame = numGame;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public Calendar getHour() {
+        return hour;
+    }
+
+    public void setHour(Calendar hour) {
+        this.hour = hour;
+    }
+
+    public Integer getArena() {
+        return arena;
+    }
+
+    public void setArena(Integer arena) {
+        this.arena = arena;
+    }
+
+    public Integer getSeason() {
+        return Season;
+    }
+
+    public void setSeason(Integer Season) {
+        this.Season = Season;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.numGame);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (!Objects.equals(this.numGame, other.numGame)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+}
